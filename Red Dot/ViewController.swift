e658520de3da7e9ba992e954b048fdb3b49d23fa
Ziad Ali <ziadali147@gmail.com>
2016-01-30 03:29:04 -0500

    Initial Commit

---
+++ Red Dot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Red Dot
+//
+//  Created by Ziad Ali on 1/30/16.
+//  Copyright © 2016 ZiadCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
